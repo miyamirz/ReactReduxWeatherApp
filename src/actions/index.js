d8c84274222b5d5da7c081af68bec5099e2c9855
Bademiya <miyamirza.esc@gmail.com>
2018-01-13 19:44:09 +0530

    Promises Continued

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,9 +1,13 @@
+import axios from 'axios';
 const API_KEY='74242fdad656163cfd551631e1e4c44d';
-const ROOT_URL=`api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+const ROOT_URL=`http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
 export const FETCH_WEATHER = 'FETCH_WEATHER';
 export function fetchWeather(city){
-  const url=`${ROOT_URL}&q={city},in`;
+  const url=`${ROOT_URL}&q=${city},us`;
+ 
   const request = axios.get(url);
+  console.log(url);
+console.log(request);
   return {
       type:FETCH_WEATHER,
       payload:request
